6d23433a7c424874c8c8011f73c67963934a95f3
Kaung Myat Han <arkarhtethan@gmail.com>
2021-10-13 23:29:24 +0630

    Typeorm initialized

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,36 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { PostModule } from './post/post.module';
+import * as Joi from 'joi';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+console.log(process.env.NODE_ENV)
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('dev', 'production', 'test')
+          .required(),
+        DB_HOST: Joi.string(),
+        DB_PORT: Joi.string(),
+        DB_USERNAME: Joi.string(),
+        DB_PASSWORD: Joi.string(),
+        DB_NAME: Joi.string(),
+      }),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: true,
+    }),
+    PostModule
+  ],
 })
-export class AppModule {}
+export class AppModule { }
