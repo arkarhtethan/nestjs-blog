478e9c91232a1e23b5b6ecc026a5f239d974218f
Kaung Myat Han <arkarhtethan@gmail.com>
2021-10-13 23:37:14 +0630

    core output / entity added

---
+++ backend/common/entities/core.entity.ts
@@ -0,0 +1,17 @@
+import { CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+export class CoreEntity {
+
+    @PrimaryGeneratedColumn()
+    @Field(type => Number)
+    id: number;
+
+    @CreateDateColumn()
+    @Field(type => Date)
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    @Field(type => Date)
+    updatedAt: Date;
+
+}
