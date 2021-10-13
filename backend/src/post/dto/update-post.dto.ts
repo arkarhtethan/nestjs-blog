3f8b6fc3f1fc8d109e773d35e2c1781ea02d7ecb
Kaung Myat Han <arkarhtethan@gmail.com>
2021-10-14 00:31:28 +0630

    update post output / dto added

--- backend/src/post/dto/update-post.dto.ts
+++ backend/src/post/dto/update-post.dto.ts
@@ -1,4 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { CoreOutput } from 'src/common/dtos/core.output';
+import { Post } from '../entities/post.entity';
 import { CreatePostDto } from './create-post.dto';
 
-export class UpdatePostDto extends PartialType(CreatePostDto) {}
+export class UpdatePostDto extends PartialType(CreatePostDto) { }
+
+export class UpdatePostOutput extends CoreOutput {
+    post?: Post;
+}
