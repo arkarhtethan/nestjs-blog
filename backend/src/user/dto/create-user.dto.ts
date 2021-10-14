d17e3cbc4898f6de664758fa7efb3a5b4026b1d3
Kaung Myat Han <arkarhtethan@gmail.com>
2021-10-14 22:02:22 +0630

    create user dto added

--- backend/src/user/dto/create-user.dto.ts
+++ backend/src/user/dto/create-user.dto.ts
@@ -1,1 +1,15 @@
-export class CreateUserDto {}
+import { PickType } from "@nestjs/mapped-types";
+import { CoreOutput } from "src/common/dtos/core.output";
+import { User } from "../entities/user.entity";
+
+export class CreateUserDto extends PickType(User, [
+    'name',
+    'password',
+    'email',
+    'phonenumber',
+]) { }
+
+export class CreateUserOutput extends CoreOutput {
+    user?: User;
+    token?: string;
+}
