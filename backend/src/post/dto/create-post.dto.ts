82b4cbd72202188376c499e2e234af6566408acf
Kaung Myat Han <arkarhtethan@gmail.com>
2021-10-14 14:38:32 +0630

    remove slug field form createpostDTO

--- backend/src/post/dto/create-post.dto.ts
+++ backend/src/post/dto/create-post.dto.ts
@@ -1,10 +1,9 @@
-import { PickType } from "@nestjs/mapped-types";
-import { CoreOutput } from "src/common/dtos/core.output";
-import { Post } from "../entities/post.entity";
+import { PickType } from '@nestjs/mapped-types';
+import { CoreOutput } from 'src/common/dtos/core.output';
+import { Post } from '../entities/post.entity';
 
 export class CreatePostDto extends PickType(Post, [
     'title',
-    'slug',
     'description',
     'coverImage',
 ]) { }
