86adf3daeee1b6d32a6078b7e057459800f6a1ff
Kaung Myat Han <arkarhtethan@gmail.com>
2021-10-15 01:03:37 +0630

    refactored update user dto. removed password field

--- backend/src/user/dto/update-user.dto.ts
+++ backend/src/user/dto/update-user.dto.ts
@@ -1,9 +1,9 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { OmitType, PartialType } from '@nestjs/mapped-types';
 import { CoreOutput } from 'src/common/dtos/core.output';
 import { User } from '../entities/user.entity';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) { }
+export class UpdateUserDto extends PartialType(OmitType(CreateUserDto, ['password'])) { }
 
 export class UpdateUserOutput extends CoreOutput {
     user?: User;
