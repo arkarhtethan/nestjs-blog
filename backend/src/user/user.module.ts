d5d73ce093efd0d1b232a3615303548a8dfa6963
Kaung Myat Han <arkarhtethan@gmail.com>
2021-10-14 22:04:11 +0630

    user repository and jwt module imported

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { JwtModule } from 'src/jwt/jwt.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User]), JwtModule,],
   controllers: [UserController],
   providers: [UserService]
 })
-export class UserModule {}
+export class UserModule { }
