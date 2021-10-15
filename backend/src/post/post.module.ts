a10270f2bd87c03664a2a64a174d952956bee52d
Kaung Myat Han <arkarhtethan@gmail.com>
2021-10-15 14:52:35 +0630

    tag and category repostiry added

--- backend/src/post/post.module.ts
+++ backend/src/post/post.module.ts
@@ -3,9 +3,11 @@ import { PostService } from './post.service';
 import { PostController } from './post.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Post } from './entities/post.entity';
+import { Tag } from './entities/tag.entity';
+import { Category } from './entities/category.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Post])],
+  imports: [TypeOrmModule.forFeature([Post, Tag, Category])],
   controllers: [PostController],
   providers: [PostService]
 })
