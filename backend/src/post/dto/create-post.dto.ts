9e4f6a8cc7b52f56562498f3420c33584b66a2fb
Kaung Myat Han <arkarhtethan@gmail.com>
2021-10-15 14:51:42 +0630

    category and tag field added for dto

--- backend/src/post/dto/create-post.dto.ts
+++ backend/src/post/dto/create-post.dto.ts
@@ -1,4 +1,6 @@
 import { PickType } from '@nestjs/mapped-types';
+import { Type } from 'class-transformer';
+import { IsArray, IsString } from 'class-validator';
 import { CoreOutput } from 'src/common/dtos/core.output';
 import { Post } from '../entities/post.entity';
 
@@ -6,7 +8,15 @@ export class CreatePostDto extends PickType(Post, [
     'title',
     'description',
     'coverImage',
-]) { }
+]) {
+    @IsString()
+    @Type(() => String)
+    category: string;
+
+    @IsArray()
+    @Type(() => String)
+    tags: string[];
+}
 
 export class CreatePostOutput extends CoreOutput {
     post?: Post;
