b8b02aa6398dcd5eeac9da72907fc6845363d868
Kaung Myat Han <arkarhtethan@gmail.com>
2021-10-16 00:31:05 +0630

    datavalidation added

--- backend/src/post/dto/get-post.dto.ts
+++ backend/src/post/dto/get-post.dto.ts
@@ -1,9 +1,11 @@
 import { Type } from "class-transformer";
+import { IsNumber } from "class-validator";
 import { CoreOutput } from "src/common/dtos/core.output";
 import { Post } from "../entities/post.entity";
 
 export class GetPostDTO {
     @Type(() => Number)
+    @IsNumber()
     id: number;
 }
 
