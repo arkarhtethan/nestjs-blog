1384a8e44dbdcbe62bfdfc8ecd186d1583544892
Kaung Myat Han <arkarhtethan@gmail.com>
2021-10-16 15:24:48 +0630

    get post by category dto and output added

---
+++ backend/src/post/dto/get-post-by-category.dto.ts
@@ -0,0 +1,14 @@
+import { Type } from "class-transformer";
+import { IsString } from "class-validator";
+import { CoreOutput } from "src/common/dtos/core.output";
+import { Post } from "../entities/post.entity";
+
+export class GetPostByCategoryDto {
+    @IsString()
+    @Type(() => String)
+    slug: string;
+}
+
+export class GetPostByCategoryOutput extends CoreOutput {
+    posts?: Post[];
+}
