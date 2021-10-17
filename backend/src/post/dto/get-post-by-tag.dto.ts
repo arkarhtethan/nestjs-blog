65e5d2d1e3a1881bd3d0f983adfe12baba3f8214
Kaung Myat Han <arkarhtethan@gmail.com>
2021-10-17 20:01:24 +0630

    GetPostByTagQueryInput and changed getPostbytagoutput

--- backend/src/post/dto/get-post-by-tag.dto.ts
+++ backend/src/post/dto/get-post-by-tag.dto.ts
@@ -1,6 +1,7 @@
 import { Type } from "class-transformer";
 import { IsString } from "class-validator";
 import { CoreOutput } from "src/common/dtos/core.output";
+import { PaginationInput, PaginationOutput } from "src/common/dtos/pagination.output";
 import { Post } from "../entities/post.entity";
 
 export class GetPostByTagParamDto {
@@ -9,6 +10,12 @@ export class GetPostByTagParamDto {
     slug: string;
 }
 
+export class GetPostByTagQueryInput extends PaginationInput { }
+
+export class GetPostByTag extends PaginationOutput {
+    posts: Post[];
+}
+
 export class GetPostByTagOutput extends CoreOutput {
-    posts?: Post[];
+    data?: GetPostByTag;
 }
