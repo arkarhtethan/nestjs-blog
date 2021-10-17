c20e4c7fa65a447f5f3e1ed45538e298363e2f41
Kaung Myat Han <arkarhtethan@gmail.com>
2021-10-17 13:07:07 +0630

    getPostInput added and getPostOutput format changed

--- backend/src/post/dto/get-posts.dto.ts
+++ backend/src/post/dto/get-posts.dto.ts
@@ -1,6 +1,26 @@
+import { Type } from "class-transformer";
+import { IsNumber, IsOptional } from "class-validator";
 import { CoreOutput } from "src/common/dtos/core.output";
+import { PaginationOutput } from "src/common/dtos/pagination.output";
 import { Post } from "../entities/post.entity";
 
+export class GetPostsInput {
+    @IsNumber()
+    @Type(() => Number)
+    @IsOptional()
+    pageNumber?: number;
+
+    @IsNumber()
+    @Type(() => Number)
+    @IsOptional()
+    limit?: number;
+}
+
+
+class GetPostsData extends PaginationOutput {
+    posts: Post[];
+}
+
 export class GetPostsOutput extends CoreOutput {
-    posts?: Post[];
+    data?: GetPostsData;
 }
