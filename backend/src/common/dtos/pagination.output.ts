8e3fa66d73e3181dca5387f6011437cf36e8e776
Kaung Myat Han <arkarhtethan@gmail.com>
2021-10-17 13:36:15 +0630

    paginationInput added

--- backend/src/common/dtos/pagination.output.ts
+++ backend/src/common/dtos/pagination.output.ts
@@ -1,5 +1,20 @@
+import { Type } from "class-transformer";
+import { IsNumber, IsOptional } from "class-validator";
+
 export class PaginationOutput {
     pages: number;
     currentPage: number;
     count: number;
+}
+
+export class PaginationInput {
+    @IsNumber()
+    @Type(() => Number)
+    @IsOptional()
+    pageNumber?: number;
+
+    @IsNumber()
+    @Type(() => Number)
+    @IsOptional()
+    limit?: number;
 }
