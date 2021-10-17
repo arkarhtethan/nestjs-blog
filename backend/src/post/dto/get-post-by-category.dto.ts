32480d4356e2fbedd7cdf1e7b82700aff5e77b87
Kaung Myat Han <arkarhtethan@gmail.com>
2021-10-17 13:40:37 +0630

    getPostByCategoryOutput modified

--- backend/src/post/dto/get-post-by-category.dto.ts
+++ backend/src/post/dto/get-post-by-category.dto.ts
@@ -1,6 +1,7 @@
 import { Type } from "class-transformer";
 import { IsString } from "class-validator";
 import { CoreOutput } from "src/common/dtos/core.output";
+import { PaginationInput, PaginationOutput } from "src/common/dtos/pagination.output";
 import { Post } from "../entities/post.entity";
 
 export class GetPostByCategoryParamDto {
@@ -9,6 +10,11 @@ export class GetPostByCategoryParamDto {
     slug: string;
 }
 
+export class GetPostByCateogryQueryDto extends PaginationInput { }
+
+class GetPostsData extends PaginationOutput {
+    posts: Post[];
+}
 export class GetPostByCategoryOutput extends CoreOutput {
-    posts?: Post[];
+    data?: GetPostsData;
 }
