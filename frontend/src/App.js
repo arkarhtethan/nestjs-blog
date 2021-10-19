41bc16a744f84cf5509af366f6039f99525c7a0a
Kaung Myat Han <arkarhtethan@gmail.com>
2021-10-19 20:06:23 +0630

    removed app.csss and routes added in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,36 @@
-import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Footer } from './components/footer/footer';
+import { Header } from './components/header/header';
+import { About } from './container/about';
+import { Contact } from './container/contact';
+import { Home } from './container/home';
+import { Register } from './container/register';
+import { Login } from './container/login';
 
 function App () {
   return (
-    <div className="App">
-      app
+    <div className="h-screen flex flex-col">
+      <BrowserRouter>
+        <Header />
+        <div className="flex-grow">
+          <Route path="/" exact >
+            <Home />
+          </Route>
+          <Route path="/about" exact >
+            <About />
+          </Route>
+          <Route path="/contact" exact >
+            <Contact />
+          </Route>
+          <Route path="/login" exact >
+            <Login />
+          </Route>
+          <Route path="/register" exact >
+            <Register />
+          </Route>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
