dce1a35906ed5fbfa5a99d2509324135cb622c51
Kaung Myat Han <arkarhtethan@gmail.com>
2021-10-19 23:24:07 +0630

    error component added

---
+++ frontend/src/components/error/formError.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function FormError ({ message }) {
+    return (
+        <small className="mb-3 text-xs text-white font-bold py-2 px-4 bg-red-500">{message}</small>
+    )
+}
+
+export const FormInputError = ({ message }) => (<small className="mb-3 ml-2 text-xs text-red-500">{message}</small>)
