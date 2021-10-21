fdc09e5dd55133fbf743a632a3f0a69d8a79524d
Kaung Myat Han <arkarhtethan@gmail.com>
2021-10-21 17:35:54 +0630

    useCategoriesData hook added

--- frontend/src/hooks/usePostsData.js
+++ frontend/src/hooks/usePostsData.js
@@ -1,7 +1,16 @@
+import { useQuery } from 'react-query';
+import { fetchCategories, fetchTags } from '../services/posts.service';
 
 export const useTagsData = () => {
     return useQuery(
         'tags',
         fetchTags,
+    );
+}
+
+export const useCategoriesData = () => {
+    return useQuery(
+        'categories',
+        fetchCategories,
     );
 }
