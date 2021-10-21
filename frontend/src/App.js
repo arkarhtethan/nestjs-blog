bd4dc2adaebccc9a69cd494d8f434fb9a80bc9cc
Kaung Myat Han <arkarhtethan@gmail.com>
2021-10-21 17:31:30 +0630

    react-query set up added in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,32 +6,43 @@ import { Contact } from './container/contact';
 import { Home } from './container/home';
 import { Register } from './container/register';
 import { Login } from './container/login';
+import PostDetails from './components/post/postDetails';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools'
+
+const queryClient = new QueryClient();
 
 function App () {
   return (
-    <div className="h-screen flex flex-col">
-      <BrowserRouter>
-        <Header />
-        <div className="flex-grow">
-          <Route path="/" exact >
-            <Home />
-          </Route>
-          <Route path="/about" exact >
-            <About />
-          </Route>
-          <Route path="/contact" exact >
-            <Contact />
-          </Route>
-          <Route path="/login" exact >
-            <Login />
-          </Route>
-          <Route path="/register" exact >
-            <Register />
-          </Route>
-        </div>
-        <Footer />
-      </BrowserRouter>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="h-screen flex flex-col">
+        <BrowserRouter>
+          <Header />
+          <div className="flex-grow">
+            <Route path="/" exact >
+              <Home />
+            </Route>
+            <Route path="/about" exact >
+              <About />
+            </Route>
+            <Route path="/contact" exact >
+              <Contact />
+            </Route>
+            <Route path="/login" exact >
+              <Login />
+            </Route>
+            <Route path="/register" exact >
+              <Register />
+            </Route>
+            <Route path="/post/:id" exact >
+              <PostDetails />
+            </Route>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </div>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
