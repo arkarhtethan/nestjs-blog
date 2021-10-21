75e7e7029a592940d4b6c9355f18f0f45ef32e96
Kaung Myat Han <arkarhtethan@gmail.com>
2021-10-21 17:14:01 +0630

    renamed FormInputError to ErrorMessage

--- frontend/src/components/error/formError.jsx
+++ frontend/src/components/error/formError.jsx
@@ -6,4 +6,4 @@ export default function FormError ({ message }) {
     )
 }
 
-export const FormInputError = ({ message }) => (<small className="mb-3 ml-2 text-xs text-red-500">{message}</small>)
+export const ErrorMessage = ({ message }) => (<small className="mb-3 ml-2 text-xs text-red-500">{message}</small>)
